33195a154069c5af720538aca5893fae1b6f14d4
Zunnoorain Rafi <bhoshyl@gmail.com>
2024-04-28 00:24:29 +0500

    registerController completed

--- backend/src/controllers/userControllers.ts
+++ backend/src/controllers/userControllers.ts
@@ -1,42 +1,41 @@
 import { Request, Response } from 'express';
 import { pool } from '../utils/db';
 import { userSchema } from '../utils/schemas';
+import bcryptjs from 'bcryptjs';
 
 // @desc Register User POST
 // @route /api/user/register
 // @access Public
 export const registerController = async (req: Request, res: Response) => {
+  const db = await pool.connect();
   try {
     const validation = userSchema.safeParse(req.body);
     if (!validation.success) {
       return res.status(400).json(validation.error.flatten().fieldErrors);
     }
     const { name, email, password } = validation.data;
-    const db = await pool.connect();
-    const userExists = await db.query(
-      `SELECT * FROM users WHERE email=${email}`
-    );
-    if (userExists) {
-      res.status(400).json({ message: 'User already exists' });
+    const userExists = await db.query('SELECT * FROM users WHERE email= $1', [
+      email,
+    ]);
+    if (userExists.rows[0]) {
+      return res.status(400).json({ message: 'User already exists' });
     }
 
-    const query = {
-      name: 'insert-user',
-      text: 'INSERT INTO users(name, email, password) VALUES($1, $2, $3) RETURNING *',
-      values: [name, email, password],
-    };
-    // const newUser = await db.query(
-    //   `INSERT INTO users(name, email, password) VALUES($1, $2, $3)`,
-    //   [name, email, password]
-    // );
-    const newUser = await db.query(query);
+    const hashedPassword = await bcryptjs.hash(password, 10);
+
+    const newUser = await db.query(
+      'INSERT INTO users(name, email, password) VALUES ($1,$2,$3) RETURNING *',
+      [name, email, hashedPassword]
+    );
+    db.release();
     if (newUser) {
-      res.status(200).json(newUser.rows[0]);
+      return res.status(200).json(newUser.rows[0]);
     } else {
-      res.status(401).json({ message: 'User not created' });
+      return res.status(401).json({ message: 'User not created' });
     }
   } catch (error) {
-    res.status(500).json({ message: 'Something went wrong' });
+    db.release();
     console.log(error);
+    return res.status(500).json({ message: 'Something went wrong' });
   }
 };
