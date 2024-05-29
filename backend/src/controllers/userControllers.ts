4ae70d963647c1488673dfa65cc16084fee29caa
Zunnoorain Rafi <zunnoorainrafi236@gmail.com>
2024-05-29 23:10:29 +0500

    login route modified

--- backend/src/controllers/userControllers.ts
+++ backend/src/controllers/userControllers.ts
@@ -28,18 +28,22 @@ export const registerController = async (req: Request, res: Response) => {
       'INSERT INTO users(name, email, password) VALUES ($1,$2,$3) RETURNING *',
       [name, email, hashedPassword]
     );
-    db.release();
-    if (newUser[0]) {
-      const token = genToken(newUser[0].id);
-      res.cookie('auth_token', token);
-      return res.status(200).json(newUser.rows[0]);
+    if (newUser.rows[0]) {
+      const token = genToken(newUser.rows[0].id);
+      res.cookie('auth_token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 86400000, // in milliseconds
+      });
+      return res.status(200).json({ message: 'User Created' });
     } else {
       return res.status(401).json({ message: 'User not created' });
     }
   } catch (error) {
-    db.release();
     console.log(error);
     return res.status(500).json({ message: 'Something went wrong' });
+  } finally {
+    db.release();
   }
 };
 
@@ -60,15 +64,25 @@ export const loginController = async (req: Request, res: Response) => {
     const userExists = await db.query('SELECT * FROM users WHERE email = $1', [
       email,
     ]);
-    // TODO: continue from here
+
     if (
       userExists.rows[0] &&
-      (await bcryptjs.compare(password, userExists.rows[0].hashedPassword))
+      (await bcryptjs.compare(password, userExists.rows[0].password))
     ) {
+      const token = genToken(userExists.rows[0].id);
+      res.cookie('auth_token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 86400000, // in milliseconds
+      });
+      return res.status(200).json({ userId: userExists.rows[0].id });
+    } else {
+      res.status(400).json({ message: 'Invalid Credentials' });
     }
   } catch (error) {
-    db.release();
     console.log(error);
     return res.status(500).json({ message: 'Something went wrong' });
+  } finally {
+    db.release();
   }
 };
