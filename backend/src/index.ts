62c92ef6040471502ddb79e32974601a970d841b
Zunnoorain Rafi <zunnoorainrafi236@gmail.com>
2024-06-05 09:07:02 +0500

    cloudinary added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,13 @@ import 'dotenv/config';
 import usersRoute from './routes/userRoutes';
 import cookieParser from 'cookie-parser';
 import path from 'path';
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const PORT = process.env.PORT_BACKEND || 4000;
 const app = express();
@@ -12,27 +19,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    // origin: 'http://localhost:5173',
     credentials: true,
   })
 );
 
-// app.use(express.static(path.join(__dirname, '../../frontend/dist')));
-
-if (process.env.NODE_ENV === 'production') {
-  const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, '/frontend/dist')));
-
-  app.get('*', (req: Request, res: Response) =>
-    res.sendFile(path.resolve(__dirname, '/frontend/dist/index.html'))
-  );
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running....');
-  });
-}
+app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 
 app.use('/api/user', usersRoute);
+
+app.get('*', (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
+});
 
 app.listen(7000, () => {
   console.log(`Server is running on PORT ${7000}`);
